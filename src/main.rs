4c5b62155208a76a8ea819a67de990620176312a
Benjamin Faller <git@pwned.info>
2023-07-16 01:34:34 +0200

    Add proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fmt;
 use std::fs;
+use std::io;
 use std::io::Cursor;
 use std::io::Read;
 use std::path::Path;
@@ -8,6 +9,7 @@ use std::path::PathBuf;
 
 use mailparse::parse_mail;
 use mailparse::MailHeaderMap;
+use mailparse::MailParseError;
 use mailparse::ParsedMail;
 use zip::ZipArchive;
 
@@ -15,18 +17,33 @@ mod dmarc;
 mod ui;
 
 use dmarc::Feedback;
+use zip::result::ZipError;
 
 use crate::dmarc::Record;
 
 #[derive(Debug)]
 enum Error {
     MissingSubject,
+    ParseMail(MailParseError),
+    ExtractZip(ZipError),
+    ReadXmlFromZip(io::Error),
+    ReadMboxFile(PathBuf, io::Error),
+    ParseDmarcReport(serde_xml_rs::Error),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Error::MissingSubject => write!(f, "Could not parse subject from email"),
+            Error::ParseMail(e) => write!(f, "Could not parse email in mbox file: {e}"),
+            Error::ExtractZip(e) => write!(f, "Failed to extract ZIP file from email: {e}"),
+            Error::ReadXmlFromZip(e) => {
+                write!(f, "Unable to extract XML report from ZIP file: {e}")
+            }
+            Error::ReadMboxFile(path, e) => {
+                write!(f, "Could not read mbox file '{}': {}", path.display(), e)
+            }
+            Error::ParseDmarcReport(e) => write!(f, "Failed to parse XML as DMARC report: {e}"),
         }
     }
 }
@@ -34,23 +51,25 @@ impl fmt::Display for Error {
 impl std::error::Error for Error {}
 
 /// Extracts the XML file contained in the ZIP attachment of the provided email part.
-fn extract_xml_from_zip(part: &ParsedMail) -> Result<String, Box<dyn std::error::Error>> {
-    let body = part.get_body_raw()?;
+fn extract_xml_from_zip(part: &ParsedMail) -> Result<String, Error> {
+    let body = part.get_body_raw().map_err(Error::ParseMail)?;
     let cursor = Cursor::new(body.as_slice());
-    let mut archive = ZipArchive::new(cursor)?;
-    let mut xml_file = archive.by_index(0)?;
+    let mut archive = ZipArchive::new(cursor).map_err(Error::ExtractZip)?;
+    let mut xml_file = archive.by_index(0).map_err(Error::ExtractZip)?;
     let mut xml = String::new();
-    xml_file.read_to_string(&mut xml)?;
+    xml_file
+        .read_to_string(&mut xml)
+        .map_err(Error::ReadXmlFromZip)?;
     Ok(xml)
 }
 
-fn get_feedbacks_from_mbox(path: &Path) -> Result<Vec<Feedback>, Box<dyn std::error::Error>> {
-    let content = fs::read_to_string(path)?;
+fn get_feedbacks_from_mbox(path: &Path) -> Result<Vec<Feedback>, Error> {
+    let content = fs::read_to_string(path).map_err(|e| Error::ReadMboxFile(path.into(), e))?;
     // Not conformant to RFC4155
     let mut feedbacks = vec![];
     let emails = content.split("From ");
     for email in emails.skip(1) {
-        let parsed_mail = parse_mail(email.as_bytes())?;
+        let parsed_mail = parse_mail(email.as_bytes()).map_err(Error::ParseMail)?;
         let subject = parsed_mail
             .get_headers()
             .get_first_value("Subject")
@@ -62,7 +81,8 @@ fn get_feedbacks_from_mbox(path: &Path) -> Result<Vec<Feedback>, Box<dyn std::er
         match zip_part {
             Some(part) => {
                 let xml = extract_xml_from_zip(part)?;
-                let feedback: Feedback = serde_xml_rs::from_str(&xml)?;
+                let feedback: Feedback =
+                    serde_xml_rs::from_str(&xml).map_err(Error::ParseDmarcReport)?;
                 feedbacks.push(feedback);
             }
             None => eprintln!("Email does not contain a zipped report"),
@@ -102,10 +122,10 @@ fn run_aggregate(feedbacks: Vec<Feedback>) {
     println!("{table}");
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn try_main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Usage `dagger mbox_path [--aggregate]`");
+        println!("Usage `dagger mbox_file_path [--aggregate]`");
         return Ok(());
     }
 
@@ -124,4 +144,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     Ok(())
+}
+
+fn main() {
+    if let Err(e) = try_main() {
+        eprintln!("Error: {e}");
+        std::process::exit(1);
+    }
 }
